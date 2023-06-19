200e6606eff2171d9e7a5662e4943276e154e403
shok415 <ganstersss@hotmail.com>
2023-06-19 21:53:42 +0300

    Start project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeFeedComponent } from './pages/home-feed/home-feed.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeFeedComponent
   ],
   imports: [
     BrowserModule,
